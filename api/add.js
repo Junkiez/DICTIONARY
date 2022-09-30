bf42655e7be81aaef045787efa7e33e11fce5d41
Kaze <80313506+Junkiez@users.noreply.github.com>
2022-09-30 12:22:03 +0300

    Update add.js

--- api/add.js
+++ api/add.js
@@ -4,10 +4,12 @@ export default async function handler(req, res) {
   if(!(ulinks in req.body) || !(word in req.body) || !(meaning in req.body)){
     res.writeHead(200, { 'Content-Type':'text/html'});
     res.end(`<center><h1>Fields are empty</h1><center><center><a href="/">Go back</a><center>`);
+    return;
   }
   if(req.body.ulinks == "" || req.body.meaning == ""){
     res.writeHead(200, { 'Content-Type':'text/html'});
     res.end(`<center><h1>Fields are empty</h1><center><center><a href="/">Go back</a><center>`);
+    return;
   }
   const client = createClient({
     url:
